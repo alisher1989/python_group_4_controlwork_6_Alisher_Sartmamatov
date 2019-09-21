5bcd4d726922cff92d6502b12ec46d604da8980d
Alisher <shera.rashiduulu@gmail.com>
2019-09-21 12:12:04 +0600

    Created index.html

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,7 +3,7 @@ from django.forms import widgets
 from webapp.models import STATUS_CHOICES
 
 
-class ArticleForm(forms.Form):
+class GuestbookForm(forms.Form):
     author = forms.CharField(max_length=40, required=True, label='author')
     email = forms.EmailField(max_length=70, required=True, label='email')
     text = forms.CharField(max_length=3000, required=True, label='Text', widget=widgets.Textarea)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from webapp.forms import GuestbookForm
+from webapp.models import Guestbook
 
-# Create your views here.
+
+def index_view(request, *args, **kwargs):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        items = Guestbook.objects.filter(title__icontains=search_query)
+    else:
+        items = Guestbook.objects.all()
+    return render(request, 'index.html', context={
+        'items': items
+    })

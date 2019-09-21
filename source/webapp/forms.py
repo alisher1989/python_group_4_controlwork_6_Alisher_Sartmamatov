1b3f2601952d06b9c28f599ef6097a2cf5f9ce7a
Alisher <shera.rashiduulu@gmail.com>
2019-09-21 11:59:25 +0600

    Created fields of forms.py

---
+++ source/webapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import widgets
+from webapp.models import STATUS_CHOICES
+
+
+class ArticleForm(forms.Form):
+    author = forms.CharField(max_length=40, required=True, label='author')
+    email = forms.EmailField(max_length=70, required=True, label='email')
+    text = forms.CharField(max_length=3000, required=True, label='Text', widget=widgets.Textarea)
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False, label='status')

1f527b800f8713388c869d2c11d7695480e5ac13
Alisher <shera.rashiduulu@gmail.com>
2019-09-21 11:49:50 +0600

    Created fields in models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('active', 'Active'),
+    ('blocked', 'Blocked'),
+)
+class Article(models.Model):
+    author = models.CharField(max_length=50, null=False, blank=False, verbose_name='author')
+    email = models.EmailField(max_length=70, null=False, blank=False, verbose_name='email')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.author
